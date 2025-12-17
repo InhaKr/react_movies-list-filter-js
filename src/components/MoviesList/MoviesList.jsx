13a7013128bc023cfaf6197e6fc01041ab1f700c
InhaKr <inkr3891@gmail.com>
2025-12-17 17:58:26 +0200

    first commit

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,19 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
+export const MoviesList = ({ movies, query }) => (
   <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
+    {!query
+      ? movies.map(movie => <MovieCard key={movie.imdbId} movie={movie} />)
+      : movies
+          .filter(movie => {
+            const q = query.toLowerCase();
+
+            return (
+              movie.title.toLowerCase().includes(q) ||
+              movie.description.toLowerCase().includes(q)
+            );
+          })
+          .map(movie => <MovieCard key={movie.imdbId} movie={movie} />)}
   </div>
 );
